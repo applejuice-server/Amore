1df8536165797bb98eb20034eae150a6f3099ae4
Minota <34592028+xMinota@users.noreply.github.com>
2020-08-11 08:44:27 -0400

    init

---
+++ src/main/kotlin/space/minota/amore/Main.kt
@@ -0,0 +1,11 @@
+package space.minota.amore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("Amore enabled!")
+    }
+
+}
